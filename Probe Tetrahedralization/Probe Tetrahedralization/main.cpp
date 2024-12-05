02c6ba26d2934e17b77430868bd8de7fd5c84a47
Rafael S <artgrph@gmail.com>
2024-12-05 14:57:14 -0800

    setting up tetgen to be used in xcode project

--- Probe Tetrahedralization/Probe Tetrahedralization/main.cpp
+++ Probe Tetrahedralization/Probe Tetrahedralization/main.cpp
@@ -4,11 +4,12 @@
 //
 //  Created by Rafael Sabino on 12/5/24.
 //
-
+#include "tetgen.h"
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
     return 0;
 }
